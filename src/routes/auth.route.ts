d118e0dc7270604f159fd1e7da09b6a022a7f4a0
Olabosinde Oladipo <olabosindeoladipo@gmail.com>
2023-03-30 19:52:31 +0100

    fine tune deploy,ent

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,9 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { login } from "../controllers/auth.controller";
 
 const router = Router();
+router.get("/", (req: Request, res: Response) => {
+  res.send({ message: "ok" });
+});
 router.post("/login", login);
 export const AuthRoutes = router;
