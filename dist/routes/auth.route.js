7ae58d359d8931e94ea30c1e04e25941974c2074
Olabosinde Oladipo <olabosindeoladipo@gmail.com>
2023-03-30 19:14:51 +0100

    removed dist folder

--- dist/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthRoutes = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const router = express_1.Router();
-router.post("/login", auth_controller_1.login);
-exports.AuthRoutes = router;
--- dist/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRoutes = void 0;
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const router = express_1.Router();
-router.post("/", user_controller_1.create);
-exports.UserRoutes = router;
