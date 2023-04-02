110f025819e5af244e221bb3f7b4e4386b52d68d
Olabosinde Oladipo <olabosindeoladipo@gmail.com>
2023-04-02 17:46:01 +0100

    API description

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,7 +3,7 @@ import { login } from "../controllers/auth.controller";
 
 const router = Router();
 router.get("/", (req: Request, res: Response) => {
-  res.send({ message: "ok" });
+  res.send({ description: "MVPMatch Maze API v1" });
 });
 router.post("/login", login);
 export const AuthRoutes = router;
